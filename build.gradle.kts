976fc9fcd63b043cbe8c75ec1014f79e71e71d6f
Fornitx <49841725+Fornitx@users.noreply.github.com>
2025-02-21 17:43:17 +0300

    jooq version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,17 @@ subprojects {
             freeCompilerArgs.addAll("-Xjsr305=strict")
         }
     }
+
+    configurations.all {
+        resolutionStrategy.dependencySubstitution {
+            substitute(module("junit:junit"))
+                .using(module("io.quarkus:quarkus-junit4-mock:3.18.0"))
+                .because(
+                    "We don't want JUnit 4; but is an unneeded transitive of testcontainers. " +
+                        "See https://github.com/testcontainers/testcontainers-java/issues/970"
+                )
+        }
+    }
 }
 
 tasks.build {
